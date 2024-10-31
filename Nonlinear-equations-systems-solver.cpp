8c71d32b5bf0853a36c540b962e4a84fb0ecc100
Artiom <azhuchok207@gmail.com>
2024-10-31 12:57:23 +0300

    added includes and comments

--- Nonlinear-equations-systems-solver.cpp
+++ Nonlinear-equations-systems-solver.cpp
@@ -1,7 +1,9 @@
 ﻿#include<stdio.h>
+#include<tinyexpr.h>
+#include<s21_matrix.h>
 #include<eqparser.h>
 #include<yakobi_matrix.h>
 
-int main()
+int main() //Также изначально на ввод запросить у пользователя имя с файлом, где находится система уравнений и далее уже вызывать parse с этим параметром
 {
 }
